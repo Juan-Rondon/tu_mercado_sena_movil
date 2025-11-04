0c468bd8fb0241098b5c214d54f0803fb895160d
Juan-Rondon <jmrondonalva@gmail.com>
2025-11-03 22:11:09 -0500

    Se creo un componente reutilizable de CustomButton

--- MercadoSena/app/index.tsx
+++ MercadoSena/app/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import CustomButton from '@/components/buttons/CustomButton'
 import React from 'react'
+import { Text, View } from 'react-native'
 
 const App = () => {
   return (
     <View>
       <Text className='font-Opensans-bold mt-16 mx-2.5 text-5xl text-primary-500'>App</Text>
+      <CustomButton variant='text-only' className='w-1/2' color='secondary'>Click Me</CustomButton>
+      <CustomButton onPress={() => {console.log('Quiero penevaginal')}} className='w-1/3' color='tertiary'>Click Me</CustomButton>
+      <CustomButton className='w-1/4'>Click Me</CustomButton>
     </View>
   )
 }
---
+++ MercadoSena/components/buttons/CustomButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Pressable, PressableProps, Text, View } from 'react-native';
+
+interface Props extends PressableProps{
+    children: string;
+    color?: 'primary' | 'secondary' | 'tertiary';
+    className?: string;
+    variant?: 'contained' | 'text-only';
+}
+
+const CustomButton = React.forwardRef(({
+    children,
+    color = 'primary',
+    onPress,
+    onLongPress,
+    className,
+    variant = 'contained',
+}:Props, ref: React.Ref<View>
+) => {
+    const textColor = {
+        primary: 'text-primary-100',
+        secondary: 'text-secondary-500',
+        tertiary: 'text-tertiary-100',
+    }[color]
+    
+    const btnColor = {
+        primary: 'bg-primary-500',
+        secondary: 'bg-secondary-500',
+        tertiary: 'bg-tertiary-500',
+    }[color]
+
+    if (variant === 'text-only') {
+        return (
+         <Pressable className = {`p-3 ${className}`} onPress={onPress} onLongPress={onLongPress}>
+          <Text className = {`text-center ${textColor}`}>{children}</Text>
+         </Pressable>
+        )
+    }
+
+  return (
+   <Pressable className = {`p-3 rounded-md ${btnColor} active:opacity-90 ${className}`} onPress={onPress} onLongPress={onLongPress}>
+    <Text className = {`text-center ${textColor}`}>{children}</Text>
+   </Pressable>
+  )
+})
+
+export default CustomButton
