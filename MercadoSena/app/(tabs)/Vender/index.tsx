f0b7039825d4a6c503271c202e9103596b815b00
JeanCarlosAraujo <jean664678@gmail.com>
2025-12-07 15:59:58 -0500

    vista ver avanzada por jean

--- MercadoSena/app/(tabs)/Vender/index.tsx
+++ MercadoSena/app/(tabs)/Vender/index.tsx
@@ -1,10 +1,79 @@
-import React from 'react'
-import { View } from 'react-native'
+import CustomButton from '@/components/buttons/CustomButton';
+import CustomInput from '@/components/inputs/CustomInput';
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+// retornar a pagina principal
+// mandar a base de datos
+// retornar a productos propios
+
 
 const venderScreen = () => {
   return (
-    <View>
-    </View>
+    <ScrollView contentContainerStyle={{ padding: 16 }}>
+      <View >
+        <View>
+          <Text className='mt-4 mg-4 text-center text-2xl font-semibold'> Publicar Producto </Text>
+        </View>
+        
+        <View>
+          <CustomInput
+            type='text'
+            placeholder='nombre producto'
+          ></CustomInput>
+
+          <CustomInput
+            type='text'
+            placeholder='Descripcion'
+          ></CustomInput>
+
+          <CustomInput
+            type='number'
+            placeholder='Precio(COP)'
+          ></CustomInput>
+
+          <CustomInput
+            type='number'
+            placeholder='Cantidad Disponible'
+          ></CustomInput>
+
+          <CustomInput
+            placeholder='Categoria'
+          ></CustomInput>
+
+          <CustomInput
+            placeholder='Condicion'
+          ></CustomInput>
+
+          <CustomInput
+            placeholder='imagen de producto '
+          ></CustomInput>
+
+        </View>
+
+        <View>
+          <CustomButton
+            variant='contained'
+            className='bg-[#538392] mb-4'
+          >
+            <Text className='text-white text-lg font-semibold'>
+              publicar producto
+            </Text>
+          </CustomButton>
+        </View>
+         
+        <View>
+          <CustomButton
+            variant='contained'
+            className='bg-[#B1CCD2] mb-4'
+          >
+            <Text className='text-white text-lg font-semibold'>
+              cancelar
+            </Text>
+          </CustomButton>
+        </View>
+
+      </View>
+    </ScrollView>
   )
 }
 
