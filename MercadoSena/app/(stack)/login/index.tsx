bc48749a3bb6d2d283ec58404a198a7aeaa158e4
Juan-Rondon <jmrondonalva@gmail.com>
2026-01-29 20:58:40 -0500

    Cambie la baseUrl en login, register y authService para ejecutar el proyecto en mi dispositivo (Cambiar si lo requieren para sus entornos)

--- MercadoSena/app/(stack)/login/index.tsx
+++ MercadoSena/app/(stack)/login/index.tsx
@@ -12,7 +12,7 @@ import CustomInput from "@/components/inputs/CustomInput";
 import ResetPasswordSheet from "@/components/sheets/ResetPasswordSheet";
 import { saveToken } from "@/src/lib/authToken";
 
-const API_BASE_URL = "http://192.168.1.4:8000";
+const API_BASE_URL = "http://192.168.1.7:8000";
 
 const LoginScreen = () => {
   const router = useRouter();
--- MercadoSena/app/(stack)/register/index.tsx
+++ MercadoSena/app/(stack)/register/index.tsx
@@ -11,7 +11,7 @@ import CustomInput from "@/components/inputs/CustomInput";
 import ResetPasswordSheet from "@/components/sheets/ResetPasswordSheet";
 import { saveToken } from "@/src/lib/authToken";
 
-const API_BASE_URL = "http://192.168.1.4:8000";
+const API_BASE_URL = "http://192.168.1.7:8000";
 
 const RegisterScreen = () => {
   const router = useRouter();
