901a9ef32b2210d5a1d51f6fa7e634e534d15b7c
Juan-Rondon <jmrondonalva@gmail.com>
2026-02-27 10:09:04 -0500

    se ajusto el device name de login para la validacion de cuenta e ingreso al home "movile"

--- MercadoSena/app/(stack)/login/index.tsx
+++ MercadoSena/app/(stack)/login/index.tsx
@@ -47,7 +47,7 @@ const LoginScreen = () => {
   try {
     setLoading(true);
 
-    const res = await fetch(`${API_BASE_URL}/api/auth/`, {
+    const res = await fetch(`${API_BASE_URL}/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -56,7 +56,7 @@ const LoginScreen = () => {
       body: JSON.stringify({
         email: email.trim(),
         password,
-        device_name: "ExpoApp",
+        device_name: "movil",
       }),
     });
 
