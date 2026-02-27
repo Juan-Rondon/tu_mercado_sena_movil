306662ca367b42087f4ce1f91a46ed21ed5dd979
Juan-Rondon <jmrondonalva@gmail.com>
2026-02-27 09:27:30 -0500

    solo se ajusto las API_URL

--- MercadoSena/app/(stack)/verify/index.tsx
+++ MercadoSena/app/(stack)/verify/index.tsx
@@ -20,8 +20,8 @@ import {
 } from "react-native";
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
-// const API_BASE_URL = "http://192.168.1.7:8000";
-const API_BASE_URL = "http://10.32.17.129:8000";
+const API_BASE_URL = "http://192.168.1.5:8000";
+// const API_BASE_URL = "http://10.32.17.129:8000";
 
 export default function VerifyScreen() {
   const router = useRouter();
--- MercadoSena/services/authService.ts
+++ MercadoSena/services/authService.ts
@@ -2,8 +2,8 @@ import { getToken } from "@/src/lib/authToken";
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://192.168.1.7:8000/api",
-  baseURL: "http://10.32.17.129:8000/api",
+  baseURL: "http://192.168.1.5:8000/api",
+  // baseURL: "http://10.32.17.129:8000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
